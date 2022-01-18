09a4c2ccf55ce9eea15ec006a1e9004cbae617df
Winterloom67 <97798264+Winterloom67@users.noreply.github.com>
2022-01-18 13:27:18 -0500

    Update scripts.ts
    Updated for the newest slate of the pet mod

--- data/mods/outheorymons/scripts.ts
+++ data/mods/outheorymons/scripts.ts
@@ -3,5 +3,7 @@
 export const Scripts: BattleScriptsData = {
    init: function() {
       this.modData('Learnsets', 'starmie').learnset.mysticalfire = ['8L1'];
+		this.modData('Learnsets', 'thundurus').learnset.spikes = [8L1];
+		this.modData('Learnsets', 'thundurustherian').learnset.spikes = [8L1];
    },
 };
